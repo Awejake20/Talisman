95ef954458da4289f5a262d68a7f9e8ac927dd0e
Jake Pfaller <jpfaller@cern.ch>
2024-09-10 15:48:57 -0400

    Changed home structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:talisman/AppState.dart';
+import 'package:talisman/Home.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const Talisman());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class Talisman extends StatelessWidget {
+  const Talisman({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return ChangeNotifierProvider(
+      create: (context) => AppState(),
+      child: MaterialApp(
+        title: 'Talisman',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         ),
+        home: Home(),
       ),
     );
   }
